e744b8165e43e598a448dae7f93b54cd1af2a9fd
Hugo-CQ <scmea160@gmail.com>
2026-08-11 02:36:32 +0800

    Add GitHub Pages build for home inventory

---
+++ github-pages/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import Home from "../app/page";
+import "../app/globals.css";
+
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("Missing application root element");
+}
+
+createRoot(root).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+);
